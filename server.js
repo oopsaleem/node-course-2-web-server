a218d48586e9911da9c97e842fc3f19094827ca7
Khaled Algelowb <khaled.algelowb@ericsson.com>
2016-12-02 04:55:10 +0300

    Add portfolio page

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ app.use(express.static(__dirname + '/public/'))
 app.get('/', (req, res)=>{
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Hello yaw,'
   });
 });
@@ -39,7 +38,13 @@ app.get('/', (req, res)=>{
 app.get('/about', (req, res)=>{
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear(),
+  });
+});
+
+app.get('/portfolio', (req, res)=>{
+  res.render('portfolio.hbs', {
+    pageTitle: 'Portfolio Page',
+    welcomeMessage: 'This should be your portfolio history.'
   });
 });
 
